2f1a5a2d3f1681f0ffd1e142c23ba484b8b1ea2e
AlinaAprel <68186607+AlinaAprel@users.noreply.github.com>
2024-10-01 11:31:14 +0300

    add calculator day and grade

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+operator = input("Введите оператор (+, -, *, /): ")
+
+if operator == "+":
+    print(f"Результат: {num1 + num2}")
+elif operator == "-":
+    print("")
+elif operator == :
+    print()
+elif operator == :
+   print()
+else:
+    print("Неверный оператор")
