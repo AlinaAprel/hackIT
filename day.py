456ef3a14e35fd7195fd025ccc2629b9ebd35ec6
Алина Апрель <alinaaprel@MacBook-Pro-Alina.local>
2024-11-25 16:53:50 +0300

    fetch

--- day.py
+++
@@ -1,9 +0,0 @@
-hour = int(input("Введите текущий час (0-23): "))
-if  <= hour < :
-    print("Сейчас утро")
-elif  <= hour < :
-    print("Сейчас день")
-elif  <= hour < :
-    print("Сейчас вечер")
-else:
-    print("Сейчас ночь")
